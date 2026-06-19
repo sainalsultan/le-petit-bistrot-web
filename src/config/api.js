ff8cd04430849ba6395fcffc42b85a25c51ab58a
sainalsultan <sainalsultan@gmail.com>
2026-06-19 08:19:22 +0800

    update endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "https://web-production-298ea8.up.railway.app/";
+export const API_BASE_URL = "https://web-production-298ea8.up.railway.app";
 
 export const API_ENDPOINTS = {
-  chat: 'api/chat',
+  chat: '/api/chat',
 };
